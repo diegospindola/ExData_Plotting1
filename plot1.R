e0bd29c0f6064c21b39b0b8295b647f17dc706c2
diegospindola <diegocspindola@gmail.com>
2015-03-06 13:05:53 -0300

    adding files

---
+++ plot1.R
@@ -0,0 +1,27 @@
+##################### Reading data
+
+#reading header
+header <- read.csv("household_power_consumption.txt",sep=";", nrows=1, header=FALSE)
+#reading data
+data <- read.csv("household_power_consumption.txt",sep=";",header=FALSE, skip=66637,nrows=2880)
+#putting labels and data together
+colnames( data ) <- unlist(header) #tip taken from this stackoverflow.com answer: http://stackoverflow.com/a/23544256/334681
+#converting dates
+library(dplyr)
+dateTimeStr <- mutate(data, DateTime = paste(as.character(Date),as.character(Time), sep=" "))$DateTime
+data$DateTime <- strptime(dateTimeStr, format="%d/%m/%Y %H:%M:%S")
+data <- select(data, c(10, 3:9))
+
+
+##################### Setting environment up
+par(mfrow=c(1,1))
+
+
+##################### Making Plot
+
+png("plot1.png",width=480,height=480)
+hist(data$Global_active_power, xlab="Global Active Power (kilowatts)", col = "red", main = "Global Active Power")
+
+
+##################### Saving File
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,39 @@
+##################### Reading data
+
+#reading header
+header <- read.csv("household_power_consumption.txt",sep=";", nrows=1, header=FALSE)
+#reading data
+data <- read.csv("household_power_consumption.txt",sep=";",header=FALSE, skip=66637,nrows=2880)
+#putting labels and data together
+colnames( data ) <- unlist(header) #tip taken from this stackoverflow.com answer: http://stackoverflow.com/a/23544256/334681
+#converting dates
+library(dplyr)
+dateTimeStr <- mutate(data, DateTime = paste(as.character(Date),as.character(Time), sep=" "))$DateTime
+data$DateTime <- strptime(dateTimeStr, format="%d/%m/%Y %H:%M:%S")
+data <- select(data, c(10, 3:9))
+
+
+##################### Setting environment up
+Sys.setlocale("LC_TIME","English")
+par(mfrow=c(2,2))
+
+
+##################### Making Plots
+
+png("plot4.png",width=480,height=480)
+#topleft
+plot(data$DateTime, data$Global_active_power, type="l", ylab="Global Active Power", xlab="")
+#topright
+plot(data$DateTime, data$Voltage, type="l", ylab="Voltage", xlab="datetime")
+#bottomleft
+plot(data$DateTime, data$Sub_metering_1, type="n", ylab="Energy sub metering", xlab="")
+lines(data$DateTime, data$Sub_metering_1)
+lines(data$DateTime, data$Sub_metering_2, col="red")
+lines(data$DateTime, data$Sub_metering_3, col="blue")
+legend("topright", col=c("black", "red","blue"), legend=names(data[6:8]), lwd=1, bty="n")
+#bottomright
+plot(data$DateTime, data$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
+
+
+##################### Saving File
+dev.off()
